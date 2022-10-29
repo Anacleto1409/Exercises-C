068cdf6dbbc00984fd8c7d4334591f29756e54bb
Anacleto1409 <112070226+Anacleto1409@users.noreply.github.com>
2022-10-29 21:35:36 +0100

    C basics

---
+++ main.c
@@ -0,0 +1,23 @@
+//Drawing
+int main()
+{
+    printf("   /|\n");
+    printf("  / |\n");
+    printf(" /  |\n");
+    printf("/___|\n");
+    return 0;
+}
+//Variables
+int main()
+{
+    char characterName[] = "Anacleto";
+    int characterAge = 75;
+    printf("There once was a named %s.\n", characterName);
+    printf("He was %d years old.\n", characterAge);
+    characterAge = 25;
+    printf("He really liked the name %s.\n", characterName);
+    printf("But did not like being %d.\n",characterAge);
+    return 0;
+}
+//Data Types
+
